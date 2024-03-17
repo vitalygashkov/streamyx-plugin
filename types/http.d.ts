6bf177088d5f4d1cb95160eedd9bfa982d3dfad2
Vitaly Gashkov <vitalygashkov@vk.com>
2024-03-17 14:48:24 +0500

    feat: add new methods to http interface

--- types/http.d.ts
+++ types/http.d.ts
@@ -1,3 +1,12 @@
 export interface Http {
+  headers: Record<string, string>;
+  cookies: string[];
+
   fetch: typeof fetch;
+
+  appendCookies(setCookie: string | string[]): void;
+  setCookies(cookies: string[]): void;
+  setHeader(name: string, value: string): void;
+  setHeaders(headers: Record<string, string>): void;
+  removeHeader(name: string): void;
 }
