92d36b7fd07fb10e5bc0bc79e867a4d5bef928d6
Vitaly Gashkov <vitalygashkov@vk.com>
2024-04-13 14:23:00 +0500

    change args interface

--- types/args.d.ts
+++ types/args.d.ts
@@ -1,26 +1,41 @@
+import { AudioCodec, DynamicRange, VideoCodec } from 'dasha';
+
 export type RunArgs = {
   urls: string[];
-  videoHeight?: number;
-  audioQuality?: string;
+
+  videoQuality?: number;
+  videoCodec?: VideoCodec;
+  videoRange?: DynamicRange;
+  videoBitrate?: number;
+  videoDimension?: '2D' | '3D';
+
+  audioCodec?: AudioCodec;
+  audioChannels?: number;
+  audioBitrate?: number;
+
+  subtitleFormat: string;
+  subtitleLanguages: string[];
+
+  languages: string[];
+
+  skipVideo: boolean;
+  skipAudio: boolean;
+  skipSubtitles: boolean;
+  skipMux: boolean;
+
   episodes: number[];
+  retry: number;
+  connections: number;
+
   seasons: number[];
+
   movieTemplate: string;
   episodeTemplate: string;
-  connections: number;
-  retryCount: number;
-  hdr: boolean;
-  '3d': boolean;
   hardsub: boolean;
-  subtitleLanguages: string[];
-  audioLanguages: string[];
-  skipSubtitles: boolean;
-  skipAudio: boolean;
-  skipVideo: boolean;
-  skipMux: boolean;
   trimBegin?: string;
   trimEnd?: string;
   pssh?: string;
-  headers?: Record<string, string>;
+  header?: Record<string, string>;
   http2: boolean;
   debug: boolean;
   version: boolean;
